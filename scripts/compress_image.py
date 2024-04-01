131dbdbe054f99e7b2fb6d0e7d237f82da1342ce
Y. F. Zhang <yunfei_z@buaa.edu.cn>
2024-04-01 21:58:38 +0800

    docs: shanghai travel

--- scripts/compress_image.py
+++ scripts/compress_image.py
@@ -3,13 +3,13 @@ from PIL import Image
 
 def compress_images(directory, max_size):
     for filename in os.listdir(directory):
-        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".JPG"):
             filepath = os.path.join(directory, filename)
             image = Image.open(filepath)
             image.save(filepath, "JPEG", optimize=True, quality=50)
 
 # 指定目录和最大文件大小
-directory = "F:\\OneDrive\\日常coding\\yunfei.github.io\\assets\\images"
+directory = "F:\\OneDrive\\日常coding\\yunfei.github.io\\assets\\images\\shanghai"
 max_size = 2 * 1024 * 1024  # 2MB
 
 # 压缩图片
